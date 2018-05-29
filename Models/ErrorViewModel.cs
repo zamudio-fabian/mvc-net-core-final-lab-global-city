e09bd8017d2e4811a65c4230c0d3ecf05e601be4
fabian <fabian.zamudio.89@gmail.com>
2018-05-28 23:07:49 -0300

    Clean project mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvc_net_core_final_lab_global_city.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
